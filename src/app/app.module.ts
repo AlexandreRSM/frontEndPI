6ac1a6edc648ea435e02f7a482cffa6d1f67fadd
unknown <alexrodriguesmeds@gmail.com>
2021-01-21 10:22:34 -0300

    components 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreNosComponent,
+    ContatoComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
