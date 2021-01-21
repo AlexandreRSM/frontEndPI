4e36f0fc8a840c442da5bd7bb6697b18e610d9fa
Joca <jocajuni@gmail.com>
2021-01-21 15:44:08 -0300

    Criando Rotas(Task 9)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "sobre-nos", component: SobreNosComponent },
+  { path: "contato", component: ContatoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
